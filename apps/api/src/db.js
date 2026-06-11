50dacfb4f2f80fee77a836cc0c4a0ecddfca24b2
rodrigo.cota <rodrigo.cota@babelgroup.com>
2026-06-11 09:50:59 +0000

    resolver erro

--- apps/api/src/db.js
+++ apps/api/src/db.js
@@ -4,10 +4,6 @@ export const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
 const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
